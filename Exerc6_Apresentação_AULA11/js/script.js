db1ac649fd8c7a9bfe2350403d07b97dc4d35e54
João Daniel Fernandes da Costa <jdfcrj@gmail.com>
2026-08-10 16:49:17 -0300

    Inclusão do JS no código

--- Exerc6_Apresentação_AULA11/js/script.js
+++ Exerc6_Apresentação_AULA11/js/script.js
@@ -1,4 +1,4 @@
-const nomeLoja = "Minha Loja Online";
+/*const nomeLoja = "Minha Loja Online";
 
 let produto1 = "Smartphone";
 let preco1 = 1500
@@ -36,4 +36,56 @@ const exibirProduto = (produto) => {
 
 };
 
-console.log(exibirProduto(produtos[2]));
+console.log(exibirProduto(produtos[2]));*/
+
+const produtos = [
+    {
+        id:1,
+        nome:"Carrinho controle remoto",
+        preco: 340.00,
+        categoria:"Brinquedos",
+        imagem:"carrinho.jpg"
+    },
+    {
+        id:2,
+        nome:"Martelo",
+        preco: 40.00,
+        categoria:"Ferramentas",
+        imagem:"martelo.jpg"
+    },
+    {
+        id:3,
+        nome:"Smartphone",
+        preco: 1340.00,
+        categoria:"Eletrônicos",
+        imagem:"Smartphone.jpg"
+    },
+    {
+        id:4,
+        nome:"Camisa",
+        preco: 60.00,
+        categoria:"Roupas",
+        imagem:"camisa.jpg"
+    },
+    {
+        id:5,
+        nome:"Relógio",
+        preco: 690.00,
+        categoria:"Acessórios",
+        imagem:"relogio.jpg"
+    }
+
+];
+
+function listaProdutos(lista){
+    lista.forEach(produto => {
+        const{id,nome,preco,categoria,imagem } = produto
+        console.log(`Id:${id} 
+            Nome:${nome}
+            Preço:${preco}
+            Categoria:${categoria}
+            Imagem:${categoria}`);
+    });
+}
+
+listaProdutos (produtos);
